ac18434bdf73d4f5b9ea625569465ce5a2825704
Sai Kiran Dasika <kirandasika30@gmail.com>
2019-03-02 19:08:28 -0600

    final updates

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import AssistantV1 from 'watson-developer-cloud/assistant/v1';
+'use strict';
+const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
 // assistant is a global variable that holds the state for the watson
 // service api
@@ -20,35 +21,38 @@ let errResponse = (reason) => {
 };
 
 let initClients = (args) => {
-   if (args.ASSISTANT_IAM_APIKEY) {
-    assistant = new AssistantV1({
-      version: '2018-02-16',
-      iam_apikey: args.ASSISTANT_IAM_APIKEY,
-      url: args.ASSISTANT_IAM_URL
-    });
-  } else if (args.ASSISTANT_USERNAME) {
-    assistant = new AssistantV1({
-      version: '2018-02-16',
-      username: args.ASSISTANT_USERNAME,
-      password: args.ASSISTANT_PASSWORD
-    });
-  } else {
-    console.error('err? ' + 'Invalid Credentials');
-    throw new Error('Invalid Credentials');
-  }
-
-  // initialize the workspace id
-  workspaceId = args.WORKSPACE_ID;
+   return new Promise((resolve, reject) => {
+        if (args.ASSISTANT_IAM_APIKEY) {
+            assistant = new AssistantV1({
+            version: '2018-02-16',
+            iam_apikey: args.ASSISTANT_IAM_APIKEY,
+            url: args.ASSISTANT_IAM_URL
+            });
+        } else if (args.ASSISTANT_USERNAME) {
+            assistant = new AssistantV1({
+            version: '2018-02-16',
+            username: args.ASSISTANT_USERNAME,
+            password: args.ASSISTANT_PASSWORD
+            });
+        } else {
+            console.error('err? ' + 'Invalid Credentials');
+            throw new Error('Invalid Credentials');
+        }
+        
+        // initialize the workspace id
+        workspaceId = args.WORKSPACE_ID;
+        resolve();
+   });
 };
 
-let handleResponse = (watsonResponse) => {
-    console.log('sending response ' + response);
+let handleResponse = (watsonResponse, resolve) => {
     let alexaResponse;
     if (watsonResponse.output.text.length > 0) {
         // assign the first value in the array as our alexa response
         alexaResponse = watsonResponse.output.text[0];
     }
-    return { 
+    console.log('sending response ' + alexaResponse);
+    resolve({ 
         version: '1.0',
         response: {
             shouldEndSession: false,
@@ -57,7 +61,7 @@ let handleResponse = (watsonResponse) => {
                 text: alexaResponse 
             }
         }
-    };
+    });
 };
 
 let handleRequest = (request) => {
@@ -68,6 +72,11 @@ let handleRequest = (request) => {
             input: { text: input },
             workspace_id: workspaceId
         }, (err, watsonResponse) => {
+            
+            if (err)  {
+                console.log(err);
+                reject(errResponse(err));
+            }
             resolve(watsonResponse);
         });
     });
@@ -85,12 +94,16 @@ let main = (args) => {
         const rawBody = Buffer.from(args.__ow_body, 'base64').toString('ascii');
         const body = JSON.parse(rawBody);
         const request = body.request;
-
+        console.log('request received ' + JSON.stringify(request));
         initClients(args)
         .then(() => handleRequest(request))
-        .then((response) => handleResponse(response))
+        .then((watsonResponse) => handleResponse(watsonResponse, resolve))
+        .catch(err => {
+            console.error('caught error ' + err);
+            console.log(err);
+            reject(errResponse(err));
+        });
     });
 };
 
-const _main = main;
-export { _main as main };
+exports.main = main;
